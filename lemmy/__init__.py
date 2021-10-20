570591ec674ea824cb2da3d0a9b944cf7a480880
Gyorgy Orosz <gyorgy@orosz.link>
2021-10-20 13:37:21 +0200

    Added lemmatizer factory

--- lemmy/__init__.py
+++ lemmy/__init__.py
@@ -1,2 +1,12 @@
 """Initialization code for Lemmy."""
+from pathlib import Path
+from typing import Union
+
+from spacy import Language
+
 from lemmy.lemmatizer import Lemmatizer
+
+
+@Language.factory("lemmy3", default_config={"data": None})
+def create(nlp: Language, name: str, data: Union[str, Path]) -> Lemmatizer:
+    return Lemmatizer
